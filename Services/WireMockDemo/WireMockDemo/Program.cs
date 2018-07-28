171f98ce4d4ec08cd58f2ec940cbaaf9b2510b14
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2018-07-28 12:14:59 +0200

    Added WireMock demo

---
+++ Services/WireMockDemo/WireMockDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WireMock.Net.StandAlone;
+using WireMock.Settings;
+
+namespace WireMockDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting WireMock.");
+
+            var settings = new FluentMockServerSettings
+            {
+                ReadStaticMappings = true,
+                WatchStaticMappings = true,
+                StartAdminInterface = true,
+            };
+
+            StandAloneApp.Start(settings);
+
+            //var server = StandAloneApp.Start(settings);
+            //server.ReadStaticMappings("CustomMappingFolder\\GetMapping");
+            //server.WatchStaticMappings("CustomMappingFolder\\GetMapping");
+
+            Console.WriteLine("WireMock started");
+            Console.ReadLine();
+        }
+    }
+}
