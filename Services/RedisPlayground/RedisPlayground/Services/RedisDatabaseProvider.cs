5415f42a9b097ea1b6324bd40345bfbdfc677c88
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2018-08-30 19:06:04 +0200

    Added RedisDemo

---
+++ Services/RedisPlayground/RedisPlayground/Services/RedisDatabaseProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RedisPlayground.Interfaces;
+using StackExchange.Redis;
+
+namespace RedisPlayground.Services
+{
+    public class RedisDatabaseProvider : IRedisDatabaseProvider
+    {
+        
+        private ConnectionMultiplexer _redisMultiplexer;
+
+        public IDatabase GetDatabase()
+        {
+            if (_redisMultiplexer == null)
+            {
+                _redisMultiplexer = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            }
+
+            return _redisMultiplexer.GetDatabase();
+
+        }
+    }
+}
